cefca3b440734cbf30157d084e42b00461187271
Vivek Sharma <guddusharma50987@gmail.com>
2024-08-22 18:13:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,36 +6,48 @@ const restart = document.querySelector('#restart');
 const startpage = document.querySelector('.startpage');
 const startbtn = document.querySelector('#start');
 const lastpage = document.querySelector('.lastpage');
-
+const bubble = document.querySelector('#bubble')
+const body = document.querySelector('body')
 let score = 0;
 let time = 30;
 let interval;
-
+let r,g,b;
 startbtn.addEventListener('click', start);
 restart.addEventListener('click', () => location.reload());
+
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256); // Red
+    const g = Math.floor(Math.random() * 256); // Green
+    const b = Math.floor(Math.random() * 256); // Blue
+
+    return `rgba(${r}, ${g}, ${b},0.4)`;
+}
+
 
 function start() {
     startpage.style.display = 'none';
     getnewhit();
     makeBubble();
-    timer();
+
+    // timer();
 }
 
 function makeBubble() {
     let counter = '';
     if(window.outerWidth < 500){
-        for (let i = 1; i <=70; i++) {
+        for (let i = 1; i <= 70; i++) {
             const rn = Math.floor(Math.random() * 10);
-            counter += `<div id="bubble">${rn}</div>`;
+            counter += `<div id="bubble" style="background-color:${getRandomColor()};color:black;">${rn}</div>`;
         }
-    }else{
+    } else {
         for (let i = 1; i <= 96; i++) {
             const rn = Math.floor(Math.random() * 10);
-            counter += `<div id="bubble">${rn}</div>`;
+            counter += `<div id="bubble" style="background-color:${getRandomColor()}">${rn}</div>`;
         }
     }
     pbtm.innerHTML = counter;
 }
+
 
 function timer() {
     interval = setInterval(() => {
